9b336eb1e9f57803624a8aa4897c787393a4be67
AlexandreMoura1988 <alexandremoura@live.com.pt>
2021-02-23 14:32:40 +0000

    Interface - ContactForm

--- src/ui/MainForm.java
+++ src/ui/MainForm.java
@@ -1,6 +1,7 @@
 package ui;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class MainForm extends JFrame {
     private JPanel rootPanel;
@@ -14,5 +15,11 @@ public class MainForm extends JFrame {
         setContentPane(rootPanel);
         setSize(500,250);
         setVisible(true);
+
+        //Put the painel in the middle of the screen
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        setLocation(dim.width / 2 - getSize().width / 2, dim.height / 2 - getSize().height / 2 );
+
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
 }
