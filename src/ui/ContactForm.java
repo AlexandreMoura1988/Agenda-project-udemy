07ce6914ef03cd05accf28bed6664350b35eebe6
AlexandreMoura1988 <alexandremoura@live.com.pt>
2021-02-23 17:44:38 +0000

    Remove contact - Business to Repository

--- src/ui/ContactForm.java
+++ src/ui/ContactForm.java
@@ -1,5 +1,7 @@
 package ui;
 
+import business.ContactBusiness;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -14,6 +16,8 @@ public class ContactForm extends JFrame {
     private JButton buttonCancel;
     private JButton buttonSave;
 
+    private ContactBusiness contactBusiness;
+
     public ContactForm() {
 
         //Methods belongs Jframe, use to init frame for our viewers
@@ -25,6 +29,8 @@ public class ContactForm extends JFrame {
         setLocation(dim.width / 2 - getSize().width / 2, dim.height / 2 - getSize().height / 2 );
 
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        contactBusiness = new ContactBusiness();
         
         setListeners();
     }
@@ -35,6 +41,18 @@ public class ContactForm extends JFrame {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
 
+                try {
+                    String name = textName.getText();
+                    String phone = textPhone.getText();
+
+                    contactBusiness.save(name, phone);
+
+                    new MainForm();
+                    dispose();
+
+                } catch (Exception e) {
+                    JOptionPane.showMessageDialog(new JFrame(), e.getMessage());
+                }
             }
         });
 
